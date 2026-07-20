a6532e56552a4b8c077f2fe758eaacc4c456f7bd
Akash Gautam <akashogz@gmail.com>
2026-07-20 11:52:04 +0530

    fix: playwright fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['playwright-core', '@sparticuz/chromium'],
+  outputFileTracingIncludes: {
+    '/api/capture': ['./node_modules/playwright-core/**'],
+  },
 }
 
 export default nextConfig
